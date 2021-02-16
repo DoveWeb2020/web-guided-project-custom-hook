bcc02c051db80fc7ee7c74b70e0e67b17e19fc38
Josh Jacobson <joshjacobson14@gmail.com>
2021-02-15 18:32:33 -0700

    lecture work-in-progress

--- followAlong/src/hooks/useForm.js
+++ followAlong/src/hooks/useForm.js
@@ -1,1 +1,8 @@
 import { useState } from "react";
+
+export const useForm = () => {
+    const [firstName, setFirstName] = useState("");
+
+    // return value should be something *kinda* like [firstName, setFirstName]
+    return [firstName, setFirstName]
+}
