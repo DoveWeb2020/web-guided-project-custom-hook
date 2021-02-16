cb3e475b65d80b4d621d2b431ce9eb7d3bebbb59
Josh Jacobson <joshjacobson14@gmail.com>
2021-02-15 19:24:36 -0700

    lecture work-in-progress

--- followAlong/src/hooks/useLocalStorage.js
+++ followAlong/src/hooks/useLocalStorage.js
@@ -5,7 +5,12 @@ import { useState } from "react";
 // This hook composes useState, providing the exact same format and functionality as useState, while also persisting the state value to localStorage
 
 export const useLocalStorage = (initialValue) => {
+
+  // First, check localStorage. (Did the user already start this form?)
+  // If the value is already in localStorage, use it to initialize the useState call
   const [value, setValue] = useState(initialValue);
+
+
 
   return [value, setValue];
 }
