3c9980aee7e9d0c52c5fc6840ac979dfc5af83bd
Josh Jacobson <joshjacobson14@gmail.com>
2021-02-15 19:21:25 -0700

    lecture work-in-progress

--- followAlong/src/hooks/useLocalStorage.js
+++ followAlong/src/hooks/useLocalStorage.js
@@ -1,2 +1,9 @@
 // on the way into localStorage, JSON.stringify (object -> string)
 // on the way out of localStorage, JSON.parse (string -> object)
+import { useState } from "react";
+
+// This hook composes useState, providing the exact same format and functionality as useState, while also persisting the state value to localStorage
+
+export const useLocalStorage = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+}
