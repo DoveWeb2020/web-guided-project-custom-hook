d62ad84c9232c817f6435245318ff855b6a2e749
Josh Jacobson <joshjacobson14@gmail.com>
2021-02-15 19:00:06 -0700

    lecture work-in-progress

--- followAlong/src/components/SignupForm.js
+++ followAlong/src/components/SignupForm.js
@@ -24,7 +24,11 @@ const useStyles = makeStyles(theme => ({
 
 export default function SignupForm() {
   const classes = useStyles();
-  const [values, handleChanges, clearForm] = useForm();
+  const initialValues = {
+    firstName: "",
+    lastName: ""
+  }
+  const [values, handleChanges, clearForm] = useForm(intialValues);
 
   const handleSubmit = e => {
     e.preventDefault();
